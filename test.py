1760f59e3637c781b7312237f86a1619db40ae75
MechanicModel <44115716+MechanicModel@users.noreply.github.com>
2021-06-08 20:54:09 +0800

    .

--- test.py
+++ test.py
@@ -1,9 +1,1 @@
-import nibabel
-import cv2
-image_array = nibabel.load('../SrcData/Label/mask-1.nii.gz').get_fdata()
-data = image_array[:,:,0]
-print(data)
-cv2.imshow(data)
-data = data.astype('uint8')
-print(data)
-cv2.imshow(data)
+print(1+1)
