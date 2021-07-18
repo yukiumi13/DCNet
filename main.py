c5d13d799b69c8188b273c3681d780326a4a7a9f
MechanicModel <44115716+MechanicModel@users.noreply.github.com>
2021-07-18 09:03:58 +0800

    .

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def train(data):
     ssm = single_salicency_model(drop_rate=0.2, layers=12)
     # ssm = torch.nn.DataParallel(ssm, device_ids=[0, 1]) 分布式训练
     ssm.cuda()
-    # ssm.load_state_dict(torch.load('bmvc_cv_single.pth'))
+    ssm.load_state_dict(torch.load('bmvc_cv_single.pth'))
     ssm = ssm.train()
     torch.backends.cudnn.enabled = True
     torch.backends.cudnn.benchmark = True
