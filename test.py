5e664883f1c90b15d56f7a700fcf64516de3637f
MechanicModel <44115716+MechanicModel@users.noreply.github.com>
2021-07-30 20:07:51 +0800

    .

--- test.py
+++ test.py
@@ -14,5 +14,5 @@ from main import *
 samplePath = os.listdir('../testset/sample')
 labelPath = os.listdir('../testset/label')
 data1 = ImageDataset('../testset/sample/' + samplePath[0], '../testset/label/' + labelPath[0])
-data = DataLoader(data1, batch_size=4, shuffle=True, pin_memory=False)
+data = DataLoader(data1, batch_size=1, shuffle=True, pin_memory=False)
 test(data)
