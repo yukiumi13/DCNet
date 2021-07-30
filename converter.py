22734c99c6026e808a8a5c33115c5482a7cf2fc6
MechanicModel <44115716+MechanicModel@users.noreply.github.com>
2021-07-30 21:27:38 +0800

    .

--- converter.py
+++ converter.py
@@ -5,7 +5,7 @@ import torch, torchvision
 from collections import OrderedDict
 dict = torch.load('bmvc_cv.pth',map_location=torch.device('cpu'))
 new_dict = OrderedDict()
-for k, v in new_dict.items():
+for k, v in dict.items():
     name = k[7:]
     new_dict[name] = v
 torch.save(new_dict,'bmvc_cv_single.pth')
