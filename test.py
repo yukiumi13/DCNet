b7a588108a7ac84e4353c11223af68d868b0f3c2
MechanicModel <44115716+MechanicModel@users.noreply.github.com>
2021-07-30 18:32:33 +0800

    .

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
+# -*-coding:utf-8 -*-
+'''
+@File    :   test.py
+@Time    :   2021/07/30 18:32:09
+@Author  :   Li Yang 
+@Version :   1.0
+@Contact :   liyang259@mail2.sysu.edu.cn
+@License :   (C)Copyright 2020-2021, Li Yang
+@Desc    :   None
+'''
+
 from main import *
 
 samplePath = os.listdir('../testset/sample')
