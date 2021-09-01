d615f693dc332cfa6c92271adb1a5d1325ff407d
MechanicModel <44115716+MechanicModel@users.noreply.github.com>
2021-09-01 23:18:16 +0800

    .

--- auxiliary_functions.py
+++ auxiliary_functions.py
@@ -344,12 +344,10 @@ def diceCal(yp, gt):
     return dice
 
 def dice_coef(output, target):#output为预测结果 target为真实结果
-    smooth = 1e-5 #防止0除
+    smooth = 1e-12
 
-    if torch.is_tensor(output):
-        output = torch.sigmoid(output).data.cpu().numpy()
-    if torch.is_tensor(target):
-        target = target.data.cpu().numpy()
+    output = output.data.cpu().numpy()
+    target = target.data.cpu().numpy()
 
     intersection = (output * target).sum()
 
