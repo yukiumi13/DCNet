06e104ac72afc003c350e8e26fb1c9bd801affca
MechanicModel <44115716+MechanicModel@users.noreply.github.com>
2021-09-15 11:07:27 +0800

    .

--- cvt.py
+++ cvt.py
@@ -1,3 +1,3 @@
 from utility import *
 for i in [22,24,25,26,29,31,33,37,40]:
-    cvt_dcm_nii('../test_data/sample'+str(i), '../test_data/'+str(i)+'.nii.gz')
+    cvt_dcm_nii('../test_data/sample/sample'+str(i), '../test_data/'+str(i)+'.nii.gz')
